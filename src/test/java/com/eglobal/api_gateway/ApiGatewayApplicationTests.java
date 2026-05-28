c927b06d5ce2de913dc8beb9db9ea37c456efcf5
MARCOK0771N <marco_charmed89@hotmail.com>
2026-05-28 02:06:10 -0600

    Initial commit

---
+++ src/test/java/com/eglobal/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eglobal.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
